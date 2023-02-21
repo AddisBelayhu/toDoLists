24f4f43a5da0be5b8b673a0a91c1bc1f4254efc8
Addis Belayhun <107222638+AddisBelayhu@users.noreply.github.com>
2023-02-21 15:34:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
-
-
-
+const list = document.querySelector("ul");
+list.addEventListener(
+    "click",
+    (ev) => {
+        if (ev.target.tagName === "LI"){
+            ev.target.classList.toggle("done");
+        }
+    },
+    false
+);
